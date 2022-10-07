4ea50749c86c302c39992932812a2e1a3d780d3b
waterbinnn <ewaterbin@naver.com>
2022-10-07 18:21:35 +0900

    Feature: 무한스크롤 구현

--- base-infinite-swapi/src/species/InfiniteSpecies.jsx
+++ base-infinite-swapi/src/species/InfiniteSpecies.jsx
@@ -1,13 +1,41 @@
-import InfiniteScroll from "react-infinite-scroller";
-import { Species } from "./Species";
+import InfiniteScroll from 'react-infinite-scroller';
+import { useInfiniteQuery } from 'react-query';
 
-const initialUrl = "https://swapi.dev/api/species/";
+import { Species } from './Species';
+
+const initialUrl = 'https://swapi.dev/api/species/';
 const fetchUrl = async (url) => {
   const response = await fetch(url);
   return response.json();
 };
 
 export function InfiniteSpecies() {
-  // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const { data, fetchNextPage, hasNextPage, isLoading, isError, error } =
+    useInfiniteQuery(
+      'sw-species',
+      ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+      {
+        getNextPageParam: (lastPage) => lastPage.next || undefined,
+      }
+    );
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error! {error.toString()}</div>;
+
+  return (
+    <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+      {data.pages.map((pageData) => {
+        return pageData.results.map((person) => {
+          return (
+            <Species
+              key={person.name}
+              name={person.name}
+              language={person.language}
+              averageLifespan={person.average_lifespan}
+            />
+          );
+        });
+      })}
+    </InfiniteScroll>
+  );
 }
