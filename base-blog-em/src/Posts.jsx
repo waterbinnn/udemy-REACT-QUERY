9f97d285bc28d1f406f6fc5d9725a14e51fe1ba4
Subin Lee <ewaterbinn@gmail.com>
2025-06-18 23:39:51 +0900

    섹션1 쿼리 생성 및 로딩/에러 상태

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,15 +1,48 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import { fetchPosts, deletePost, updatePost } from "./api";
-import { PostDetail } from "./PostDetail";
+import { fetchPosts, deletePost, updatePost } from './api';
+import { PostDetail } from './PostDetail';
+import { useQuery } from '@tanstack/react-query';
 const maxPostPage = 10;
 
 export function Posts() {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  // replace with useQuery
-  const data = [];
+  const { data, error, isError, isLoading } = useQuery({
+    queryKey: ['posts'], //쿼리 캐시 내의 데이터 정의
+    queryFn: fetchPosts, //데이터를 가져오기 위해 실행할 함수 - 비동기 함수라 가져오는데에 시간이 걸림. 그동안 데이터가 없음.
+    staleTime: 2000, // ms 데이터를 다시 가져와야 할 때를 알려줌
+    // gcTime: 4000,
+  });
+
+  if (isLoading) {
+    /*
+    isFetching : 비동기 쿼리가 아직 해결되지 않았다는 것을 나타냄 
+    isLoading : isFetching의 하위 집합으로, 로딩중임을 나타냄. 쿼리함수 미해결 + 캐시된 데이터 X 
+    쿼리를 전에 실행한 적이 없어서 데이터를 가져오는 중임. 캐시된 데이터도 없어서 보여줄 수 없음 
+    */
+    return <h3>Loading...</h3>;
+  }
+
+  if (isError) {
+    /**
+     * fetchPosts 3번 시도후 에러 표시
+     */
+    return (
+      <h3>
+        isError! <br /> 에러 메세지도 알 수 있음 : {error.message}
+      </h3>
+    );
+  }
+
+  /**
+   * stale : '오래된' , 유효기간이 만료되었다는 뜻. 다시 가져올 준비가 된 상태. 캐시는 남아있음. 데이터를 다시 검증해야한다는 뜻.
+   * 데이터 prefetch는 stale 일때만 트리거 됨.
+   * staleTile: 데이터의 최대 수명. 서버로부터 가장 최신 데이터 버전을 가져오기 전, 데이터를 얼마나 유지할건지 판단하는 시간.
+   * gcTime : 가비지 콜렉션 time . 데이터를 캐시에 유지할 시간 (기본 5시간) 페이지에 표시된 후부터 계산
+   *
+   */
 
   return (
     <>
@@ -17,14 +50,14 @@ export function Posts() {
         {data.map((post) => (
           <li
             key={post.id}
-            className="post-title"
+            className='post-title'
             onClick={() => setSelectedPost(post)}
           >
             {post.title}
           </li>
         ))}
       </ul>
-      <div className="pages">
+      <div className='pages'>
         <button disabled onClick={() => {}}>
           Previous page
         </button>
--- base-blog-em/src/api.js
+++ base-blog-em/src/api.js
@@ -2,6 +2,7 @@ export async function fetchPosts(pageNum = 1) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
   );
+  // throw new Error('no posts');
   return response.json();
 }
 
@@ -15,7 +16,7 @@ export async function fetchComments(postId) {
 export async function deletePost(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${postId}`,
-    { method: "DELETE" }
+    { method: 'DELETE' }
   );
   return response.json();
 }
@@ -23,7 +24,7 @@ export async function deletePost(postId) {
 export async function updatePost(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${postId}`,
-    { method: "PATCH", data: { title: "REACT QUERY FOREVER!!!!" } }
+    { method: 'PATCH', data: { title: 'REACT QUERY FOREVER!!!!' } }
   );
   return response.json();
 }
