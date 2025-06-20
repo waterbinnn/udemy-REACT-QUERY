c9681668a1ed17fea7d6f6d985f029b06a401fc3
Subin Lee <ewaterbinn@gmail.com>
2025-06-20 20:45:22 +0900

    feat: home에서 treatment data를 prefetch 하도록 prefetchQuery 설정

--- base-lazy-days/client/src/components/app/Home.tsx
+++ base-lazy-days/client/src/components/app/Home.tsx
@@ -1,14 +1,18 @@
-import { Icon, Stack, Text } from "@chakra-ui/react";
-import { GiFlowerPot } from "react-icons/gi";
+import { Icon, Stack, Text } from '@chakra-ui/react';
+import { GiFlowerPot } from 'react-icons/gi';
 
-import { BackgroundImage } from "@/components/common/BackgroundImage";
+import { usePrefetchTreatments } from '../treatments/hooks/useTreatments';
+
+import { BackgroundImage } from '@/components/common/BackgroundImage';
 
 export function Home() {
+  usePrefetchTreatments(); //home이 동적인 컴포넌트는 아니라서 바로 사용
+
   return (
-    <Stack textAlign="center" justify="center" height="84vh">
+    <Stack textAlign='center' justify='center' height='84vh'>
       <BackgroundImage />
-      <Text textAlign="center" fontFamily="Forum, sans-serif" fontSize="6em">
-        <Icon m={4} verticalAlign="top" as={GiFlowerPot} />
+      <Text textAlign='center' fontFamily='Forum, sans-serif' fontSize='6em'>
+        <Icon m={4} verticalAlign='top' as={GiFlowerPot} />
         Lazy Days Spa
       </Text>
       <Text>Hours: limited</Text>
