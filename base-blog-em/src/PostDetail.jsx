3828ece57ba711accca7a5b4801c6b5895795a80
Subin Lee <ewaterbinn@gmail.com>
2025-06-20 14:44:10 +0900

    mutation 로직 작성

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,14 +1,61 @@
-import { fetchComments } from "./api";
-import "./PostDetail.css";
+import { useQuery } from '@tanstack/react-query';
+import { fetchComments } from './api';
+import './PostDetail.css';
 
-export function PostDetail({ post }) {
-  // replace with useQuery
-  const data = [];
+/**
+ */
+
+export function PostDetail({ post, deleteMutation, updateMutation }) {
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ['comments', post.id], //각각의 id를 서로 다른 쿼리로 처리
+    queryFn: () => fetchComments(post.id),
+  });
+
+  if (isLoading) {
+    return <h3>Loading</h3>;
+  }
+
+  if (isError) {
+    return (
+      <>
+        <h3>isError!</h3>
+        <p>{error.message}</p>
+      </>
+    );
+  }
 
   return (
     <>
-      <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <h3 style={{ color: 'blue' }}>{post.title}</h3>
+
+      <div>
+        <button onClick={() => deleteMutation.mutate(post.id)}>Delete</button>
+        {deleteMutation.isPending && (
+          <p className='loading'>Deleting the post</p>
+        )}
+        {deleteMutation.isError && (
+          <p className='error'>
+            Error deleting the Post : {deleteMutation.error.toString()}
+          </p>
+        )}
+        {deleteMutation.isSuccess && (
+          <p className='success'>Post was deleted</p>
+        )}
+      </div>
+
+      <div>
+        <button onClick={() => updateMutation.mutate(post.id)}>
+          Update title
+        </button>
+        {updateMutation.isPending && (
+          <p className='loading'>Loading on update</p>
+        )}
+        {updateMutation.isError && <p className='error'>Error on updating</p>}
+        {updateMutation.isSuccess && (
+          <p className='success'>Success on updating</p>
+        )}
+      </div>
+
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,48 +1,57 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { fetchPosts, deletePost, updatePost } from './api';
 import { PostDetail } from './PostDetail';
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+
 const maxPostPage = 10;
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  const { data, error, isError, isLoading } = useQuery({
-    queryKey: ['posts'], //쿼리 캐시 내의 데이터 정의
-    queryFn: fetchPosts, //데이터를 가져오기 위해 실행할 함수 - 비동기 함수라 가져오는데에 시간이 걸림. 그동안 데이터가 없음.
-    staleTime: 2000, // ms 데이터를 다시 가져와야 할 때를 알려줌
-    // gcTime: 4000,
+  const queryClient = useQueryClient();
+
+  const deleteMutation = useMutation({
+    //삭제
+    mutationFn: (postId) => {
+      //인수를 받을 수 있음 (useQuery와 다르게)
+      deletePost(postId);
+    },
+  });
+  const updateMutation = useMutation({
+    //수정
+    mutationFn: (postId) => updatePost(postId),
+  });
+
+  useEffect(() => {
+    if (currentPage < maxPostPage) {
+      const nextPage = currentPage + 1;
+      queryClient.prefetchQuery({
+        //staleTime 0초
+        queryKey: ['posts', nextPage],
+        queryFn: () => fetchPosts(nextPage),
+      });
+    }
+  }, [currentPage, queryClient]);
+
+  const { data, error, isError, isLoading, isFetching } = useQuery({
+    queryKey: ['posts', currentPage],
+    queryFn: () => fetchPosts(currentPage),
+    staleTime: 2000,
   });
 
   if (isLoading) {
-    /*
-    isFetching : 비동기 쿼리가 아직 해결되지 않았다는 것을 나타냄 
-    isLoading : isFetching의 하위 집합으로, 로딩중임을 나타냄. 쿼리함수 미해결 + 캐시된 데이터 X 
-    쿼리를 전에 실행한 적이 없어서 데이터를 가져오는 중임. 캐시된 데이터도 없어서 보여줄 수 없음 
-    */
     return <h3>Loading...</h3>;
   }
 
   if (isError) {
-    /**
-     * fetchPosts 3번 시도후 에러 표시
-     */
     return (
       <h3>
         isError! <br /> 에러 메세지도 알 수 있음 : {error.message}
       </h3>
     );
   }
-
-  /**
-   * stale : '오래된' , 유효기간이 만료되었다는 뜻. 다시 가져올 준비가 된 상태. 캐시는 남아있음. 데이터를 다시 검증해야한다는 뜻.
-   * 데이터 prefetch는 stale 일때만 트리거 됨.
-   * staleTile: 데이터의 최대 수명. 서버로부터 가장 최신 데이터 버전을 가져오기 전, 데이터를 얼마나 유지할건지 판단하는 시간.
-   * gcTime : 가비지 콜렉션 time . 데이터를 캐시에 유지할 시간 (기본 5시간) 페이지에 표시된 후부터 계산
-   *
-   */
 
   return (
     <>
@@ -51,23 +60,43 @@ export function Posts() {
           <li
             key={post.id}
             className='post-title'
-            onClick={() => setSelectedPost(post)}
+            onClick={() => {
+              updateMutation.reset();
+              deleteMutation.reset();
+              setSelectedPost(post);
+            }}
           >
             {post.title}
           </li>
         ))}
       </ul>
       <div className='pages'>
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => {
+            setCurrentPage((prev) => prev - 1);
+          }}
+        >
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => {
+            setCurrentPage((prev) => prev + 1);
+          }}
+        >
           Next page
         </button>
       </div>
       <hr />
-      {selectedPost && <PostDetail post={selectedPost} />}
+      {selectedPost && (
+        <PostDetail
+          post={selectedPost}
+          deleteMutation={deleteMutation}
+          updateMutation={updateMutation}
+        />
+      )}
     </>
   );
 }
