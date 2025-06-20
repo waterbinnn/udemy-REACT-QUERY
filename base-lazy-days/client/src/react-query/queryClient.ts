56292f3bfb39ddd3c26c6e6226ed3aaf453033b6
Subin Lee <ewaterbinn@gmail.com>
2025-06-20 15:50:42 +0900

    feat: add error handler

--- base-lazy-days/client/src/react-query/queryClient.ts
+++ base-lazy-days/client/src/react-query/queryClient.ts
@@ -1,17 +1,28 @@
-import { toast } from "@/components/app/toast";
+import { QueryCache, QueryClient } from '@tanstack/react-query';
 
-// function errorHandler(errorMsg: string) {
-//   // https://chakra-ui.com/docs/components/toast#preventing-duplicate-toast
-//   // one message per page load, not one message per query
-//   // the user doesn't care that there were three failed queries on the staff page
-//   //    (staff, treatments, user)
-//   const id = "react-query-toast";
+import { toast } from '@/components/app/toast';
 
-//   if (!toast.isActive(id)) {
-//     const action = "fetch";
-//     const title = `could not ${action} data: ${
-//       errorMsg ?? "error connecting to server"
-//     }`;
-//     toast({ id, title, status: "error", variant: "subtle", isClosable: true });
-//   }
-// }
+// onError callback에 사용되는 함수
+function errorHandler(errorMsg: string) {
+  // https://chakra-ui.com/docs/components/toast#preventing-duplicate-toast
+  // one message per page load, not one message per query
+  // the user doesn't care that there were three failed queries on the staff page
+  //    (staff, treatments, user)
+  const id = 'react-query-toast';
+
+  if (!toast.isActive(id)) {
+    const action = 'fetch';
+    const title = `could not ${action} data: ${
+      errorMsg ?? 'error connecting to server'
+    }`;
+    toast({ id, title, status: 'error', variant: 'subtle', isClosable: true });
+  }
+}
+
+export const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error) => {
+      errorHandler(error.message);
+    },
+  }),
+});
