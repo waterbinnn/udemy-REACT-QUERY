e48a7177196ab77884a248a64d4672950711c584
Subin Lee <ewaterbinn@gmail.com>
2025-06-24 11:13:58 +0900

    feat: key 중앙관리를 위한 factory 작성

---
+++ base-lazy-days/client/src/react-query/key-factories.ts
@@ -0,0 +1,9 @@
+import { queryKeys } from './constants';
+
+export const generateUserKey = (userId: number, userToken: string) => {
+  return [queryKeys.user, userId, userToken];
+};
+
+export const generateAppointmentsKey = (userId: number, userToken: string) => {
+  return [queryKeys.appointments, queryKeys.user, userId, userToken];
+};
