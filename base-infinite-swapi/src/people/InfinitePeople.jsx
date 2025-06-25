a80ac3ee7924e0cf92c218f37bd4955d98cbd510
Subin Lee <ewaterbinn@gmail.com>
2025-06-25 18:13:34 +0900

    feat: 무한스크롤 구현

--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,13 +1,62 @@
-import InfiniteScroll from "react-infinite-scroller";
-import { Person } from "./Person";
+import InfiniteScroll from 'react-infinite-scroller';
+import { Person } from './Person';
+import { useInfiniteQuery } from '@tanstack/react-query';
 
-const initialUrl = "https://swapi.dev/api/people/";
+const baseUrl = 'https://swapi-node.vercel.app';
+const initialUrl = baseUrl + '/api/people/';
+
 const fetchUrl = async (url) => {
   const response = await fetch(url);
   return response.json();
 };
 
 export function InfinitePeople() {
-  // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  // get data for InfiniteScroll via React Query
+  const { data, isLoading, isError, fetchNextPage, hasNextPage, isFetching } =
+    useInfiniteQuery({
+      queryKey: ['sw-people'],
+      queryFn: ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+      getNextPageParam: (lastPage) => {
+        //getNextPageParam - undefined 반환 여부에 따라 결정됨
+        return lastPage.next ? baseUrl + lastPage.next : undefined;
+      },
+    });
+
+  if (isLoading) {
+    return <div className='loading'>loading</div>;
+    //isFetching하면 조기반환이 되어서 스크롤이 위로 올라가버린다. 그래서 아래처럼 컴포넌트 내에 작성해주는게 좋음
+  }
+
+  if (isError) {
+    return <div className='error'>error</div>;
+  }
+
+  return (
+    <>
+      {isFetching && <div className='loading'>loading</div>}
+      <InfiniteScroll
+        loadMore={() => {
+          if (!isFetching) {
+            fetchNextPage();
+          }
+        }}
+        hasMore={hasNextPage}
+      >
+        {data.pages.map((pageData) => {
+          return pageData.results.map((person) => {
+            const { fields } = person;
+
+            return (
+              <Person
+                key={fields.name}
+                name={fields.name}
+                hairColor={fields.hair_color}
+                eyeColor={fields.eye_color}
+              />
+            );
+          });
+        })}
+      </InfiniteScroll>
+    </>
+  );
 }
--- base-infinite-swapi/src/species/InfiniteSpecies.jsx
+++ base-infinite-swapi/src/species/InfiniteSpecies.jsx
@@ -1,13 +1,62 @@
-import InfiniteScroll from "react-infinite-scroller";
-import { Species } from "./Species";
+import InfiniteScroll from 'react-infinite-scroller';
+import { Species } from './Species';
+import { useInfiniteQuery } from '@tanstack/react-query';
 
-const initialUrl = "https://swapi.dev/api/species/";
+const baseUrl = 'https://swapi-node.vercel.app';
+const initialUrl = baseUrl + '/api/species/';
+
 const fetchUrl = async (url) => {
   const response = await fetch(url);
   return response.json();
 };
 
 export function InfiniteSpecies() {
-  // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const { data, isLoading, isFetching, isError, hasNextPage, fetchNextPage } =
+    useInfiniteQuery({
+      queryKey: ['sw-species'],
+      queryFn: ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+      getNextPageParam: (lastPage) => {
+        return lastPage.next ? baseUrl + lastPage.next : undefined;
+      },
+    });
+
+  console.log(data.pageParams);
+
+  if (isLoading) {
+    return <div>loading</div>;
+  }
+
+  if (isError) {
+    return <div>error</div>;
+  }
+
+  return (
+    <>
+      {isFetching && <div className='loading'>loading</div>}
+      <InfiniteScroll
+        loadMore={() => {
+          //중복 요청 방지
+          if (!isFetching) {
+            fetchNextPage();
+          }
+        }}
+        hasMore={hasNextPage}
+      >
+        {data.pages.map((pageData) => {
+          return pageData.results.map((species) => {
+            const { fields } = species;
+            return (
+              <Species
+                key={fields.name}
+                name={fields.name}
+                language={fields.language}
+                averageLifespan={fields.average_lifespan}
+              />
+            );
+          });
+        })}
+      </InfiniteScroll>
+      <div>d</div>
+    </>
+  );
 }
