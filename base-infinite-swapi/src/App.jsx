460ddaa131be76e7b8d5939e30c2e565dd73bebb
Subin Lee <ewaterbinn@gmail.com>
2025-06-25 18:13:22 +0900

    deps: tanstack query setting

--- base-infinite-swapi/src/App.jsx
+++ base-infinite-swapi/src/App.jsx
@@ -1,14 +1,21 @@
-import "./App.css";
-import { InfinitePeople } from "./people/InfinitePeople";
-import { InfiniteSpecies } from "./species/InfiniteSpecies";
+import './App.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { InfinitePeople } from './people/InfinitePeople';
+import { InfiniteSpecies } from './species/InfiniteSpecies';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <h1>Infinite SWAPI</h1>
-      <InfinitePeople />
-      {/* <InfiniteSpecies /> */}
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className='App'>
+        <h1>Infinite SWAPI</h1>
+        {/* <InfinitePeople /> */}
+        <InfiniteSpecies />
+      </div>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
