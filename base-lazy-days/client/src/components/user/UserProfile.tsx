89f962f709b28de6ff94ccd6523a3feb88572c54
Subin Lee <ewaterbinn@gmail.com>
2025-06-29 21:16:56 +0900

    feat:  낙관적 업데이트 로직 작성

--- base-lazy-days/client/src/components/user/UserProfile.tsx
+++ base-lazy-days/client/src/components/user/UserProfile.tsx
@@ -7,16 +7,19 @@ import {
   Heading,
   Input,
   Stack,
-} from "@chakra-ui/react";
-import { Field, Form, Formik } from "formik";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+} from '@chakra-ui/react';
+import { useMutation, useMutationState } from '@tanstack/react-query';
+import { Field, Form, Formik } from 'formik';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import { usePatchUser } from "./hooks/usePatchUser";
-import { useUser } from "./hooks/useUser";
-import { UserAppointments } from "./UserAppointments";
+import { User } from '@shared/types';
 
-import { useLoginData } from "@/auth/AuthContext";
+import { MUTATION_KEY, usePatchUser } from './hooks/usePatchUser';
+import { useUser } from './hooks/useUser';
+import { UserAppointments } from './UserAppointments';
+
+import { useLoginData } from '@/auth/AuthContext';
 
 export function UserProfile() {
   const { userId } = useLoginData();
@@ -28,11 +31,20 @@ export function UserProfile() {
     // use login data for redirect, for base app that doesn't
     //   retrieve user data from the server yet
     if (!userId) {
-      navigate("/signin");
+      navigate('/signin');
     }
   }, [userId, navigate]);
 
-  const formElements = ["name", "address", "phone"];
+  const pendingData = useMutationState({
+    filters: { mutationKey: [MUTATION_KEY], status: 'pending' },
+    select: (mutation) => {
+      return mutation.state.variables as User;
+    },
+  });
+
+  const pendingUser = pendingData ? pendingData[0] : null;
+
+  const formElements = ['name', 'address', 'phone'];
   interface FormValues {
     name: string;
     address: string;
@@ -40,19 +52,21 @@ export function UserProfile() {
   }
 
   return (
-    <Flex minH="84vh" textAlign="center" justify="center">
-      <Stack spacing={8} mx="auto" w="xl" py={12} px={6}>
+    <Flex minH='84vh' textAlign='center' justify='center'>
+      <Stack spacing={8} mx='auto' w='xl' py={12} px={6}>
         <UserAppointments />
-        <Stack textAlign="center">
-          <Heading>Your information</Heading>
+        <Stack textAlign='center'>
+          <Heading>
+            information for {pendingUser ? pendingUser.name : user?.name}
+          </Heading>
         </Stack>
-        <Box rounded="lg" bg="white" boxShadow="lg" p={8}>
+        <Box rounded='lg' bg='white' boxShadow='lg' p={8}>
           <Formik
             enableReinitialize
             initialValues={{
-              name: user?.name ?? "",
-              address: user?.address ?? "",
-              phone: user?.phone ?? "",
+              name: user?.name ?? '',
+              address: user?.address ?? '',
+              phone: user?.phone ?? '',
             }}
             onSubmit={(values: FormValues) => {
               patchUser({ ...user, ...values });
@@ -65,7 +79,7 @@ export function UserProfile() {
                   <Field name={element} as={Input} />
                 </FormControl>
               ))}
-              <Button mt={6} type="submit">
+              <Button mt={6} type='submit'>
                 Update
               </Button>
             </Form>
