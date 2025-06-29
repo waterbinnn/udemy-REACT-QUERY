d4fb44c413991099c3fcd436a3e6b3d1fa64b30d
Subin Lee <ewaterbinn@gmail.com>
2025-06-29 21:16:22 +0900

    feat: generateUserKey 함수에서 userToken 제거

--- base-lazy-days/client/src/react-query/key-factories.ts
+++ base-lazy-days/client/src/react-query/key-factories.ts
@@ -1,7 +1,8 @@
 import { queryKeys } from './constants';
 
 export const generateUserKey = (userId: number, userToken: string) => {
-  return [queryKeys.user, userId, userToken];
+  // userToken이 없어야 함. 사용자 ID에 대한 키를 동일하게 유지하기 위함
+  return [queryKeys.user, userId];
 };
 
 export const generateAppointmentsKey = (userId: number, userToken: string) => {
